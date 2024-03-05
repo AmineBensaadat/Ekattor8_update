a7ea999d4580e4542541722da1eb129c0a1348cc
bensaadat <bensaadat.amine@gmail.com>
2024-03-05 15:45:04 +0100

    update sections list in class list

--- resources/views/admin/section/add_classe_to_section.blade.php
+++ resources/views/admin/section/add_classe_to_section.blade.php
@@ -1,15 +1,47 @@
+<br>
+<br>
 <div class="eoff-form">
-    <form method="POST" enctype="multipart/form-data" class="d-block ajaxForm" action="{{ route('admin.create.section') }}">
-        @csrf 
-        <div class="form-row">
-            {{-- <div class="fpb-7">
-                <label for="name" class="eForm-label">{{ get_phrase('Name') }}</label>
-                <input type="text" class="form-control eForm-control" id="name" name="name" required>
+    <div class="container">
+        <div class="row">
+            <div class="col"></div>
+            <div class="col-auto">
+                <button id="addClassBtn" class="btn btn-primary">Add Class</button>
             </div>
-            <div class="fpb-7 pt-2">
-                <button class="btn-form" type="submit">{{ get_phrase('Create section') }}</button>
-            </div>
-            <div class="fpb-7 pt-2"> --}}
         </div>
-    </form>
+    </div>
+    <br>
+    <div class="container">
+      <div class="row mt-3">
+        <div class="col">
+            <table id="classTable" class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Column 1</th>
+                        <th scope="col">Column 2</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Data 1</td>
+                        <td>Data 2</td>
+                    </tr>
+                    <!-- Add more rows as needed -->
+                </tbody>
+            </table>
+        </div>
+    </div>
+    </div>
 </div>
+</div>
+<script>
+    $(document).ready(function() {
+        $("#addClassBtn").click(function() {
+            var table = $("#classTable tbody");
+            var newRow = $("<tr>");
+            newRow.append("<td>New Data 1</td>");
+            newRow.append("<td>New Data 2</td>");
+            table.append(newRow);
+        });
+    });
+</script>
+
